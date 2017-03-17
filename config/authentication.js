bc1848e031c5cdc7b99aeacee356725d58209708
Mingxiao Guo <marshallguo0221@gmail.com>
2017-03-17 15:39:56 -0700

    change edit user

--- config/authentication.js
+++ config/authentication.js
@@ -4,12 +4,12 @@
 module.exports = {
   'facebookAuth' : {
     'clientID': '1481431478557686',
-    'clientSecret': '013997f066ce116fb43d49963c1045a2',
+    'clientSecret': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
     'callbackURL': "../auth/facebook/callback"
   },
   'googleAuth' : {
     'clientID': "960864811575-cln6a5h75lvoa1hcihbsck2ru7b2ijr1.apps.googleusercontent.com",
-    'clientSecret': "yTOPo-E6BQ4yEJh8iF3vq4VD",
+    'clientSecret': "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
     'callbackURL': "../auth/google/callback"
   }
 }
--- routes/profile.js
+++ routes/profile.js
@@ -5,26 +5,43 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (passport) {
-    router.get('/', isLoggedIn, function (req, res) {
-        res.render('pages/profile', { user : req.user });
-    });
+  router.get('/', isLoggedIn, function (req, res) {
+    res.render('pages/profile', { user : req.user });
+  });
 
-    router.get('/edit', function (req, res) {
-        // different login methods require different ways to edit profile
-        if (req.isAuthenticated()) {
-            res.render('pages/editUserProfile', { user : req.user});
-        } else {
-            res.redirect('/login');
-        }
-    });
+  router.get('/edit', function (req, res) {
+    // different login methods require different ways to edit profile
+
+    var user = {
+      _id: req.user._id,
+      type: "local",
+      profile: {}
+    };
+    // console.log(req.user.facebook.id);
+    if (req.user.local.email != undefined) {
+      user.profile = req.user.local;
+    } else if (req.user.facebook.id != undefined) {
+      user.profile = req.user.facebook;
+      user.type = "facebook";
+    } else {
+      user.profile = req.user.google;
+      user.type = "google";
+    }
+    console.log(user);
+    if (req.isAuthenticated()) {
+      res.render('pages/editUserProfile', { user : req.user, profileUser: user});
+    } else {
+      res.redirect('/login');
+    }
+  });
 
     return router;
 };
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        //console.log("in isLoggedIn, check req is authenticate or not");
-        return next();
-    }
-    res.redirect('/login');
+  if (req.isAuthenticated()) {
+      //console.log("in isLoggedIn, check req is authenticate or not");
+      return next();
+  }
+  res.redirect('/login');
 }
