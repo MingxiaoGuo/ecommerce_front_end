5fa91cd5877a31b75b03847898c2ce0d6de935dc
Mingxiao Guo <marshallguo0221@gmail.com>
2017-03-17 23:48:16 -0700

    fix alert bug in user profile edit page

--- config/authentication.js
+++ config/authentication.js
@@ -4,12 +4,12 @@
 module.exports = {
   'facebookAuth' : {
     'clientID': '1481431478557686',
-    'clientSecret': 'XXXXXXXXXXXXXXXXXXXX',
+    'clientSecret': 'XXXXXXXXXXXXXXXXXXXXX',
     'callbackURL': "../auth/facebook/callback"
   },
   'googleAuth' : {
     'clientID': "960864811575-cln6a5h75lvoa1hcihbsck2ru7b2ijr1.apps.googleusercontent.com",
-    'clientSecret': "XXXXXXXXXXXXXXXXXXX",
+    'clientSecret': "XXXXXXXXXXXXXXXXXXXXXXXX",
     'callbackURL': "../auth/google/callback"
   }
 }
